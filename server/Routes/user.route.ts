93ee39a00e7b4016facfd2c71e056d302fb4e243
megersam <megersabmuleta01@gmail.com>
2023-12-07 14:24:58 +0300

    update user password is done password can be update after authentication done.

--- server/Routes/user.route.ts
+++ server/Routes/user.route.ts
@@ -1,5 +1,5 @@
 import  express  from "express";
-import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken, updateUserInfo } from "../Controllers/User.Controller";
+import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken, updatePassword, updateUserInfo } from "../Controllers/User.Controller";
 import { isAuthenticated } from "../Middleware/auth";
 const userRouter = express.Router();
 
@@ -18,5 +18,7 @@ userRouter.get('/me', isAuthenticated, getUserInfo);
 userRouter.post('/social-auth', socialAuth);
 
 userRouter.put('/update-user-info', isAuthenticated, updateUserInfo);
+
+userRouter.put('/update-user-password', isAuthenticated, updatePassword);
 
 export default userRouter;
