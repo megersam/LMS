c3b06d72962bb9f50818f7501116ff2188101700
megersam <megersabmuleta01@gmail.com>
2023-12-08 15:52:39 +0300

    edit course api created

--- server/Controllers/course.Controller.ts
+++ server/Controllers/course.Controller.ts
@@ -4,25 +4,64 @@ import { CatchAsyncErrors } from "../Middleware/CatchAsyncErrors";
 import ErrorHandler from "../utils/ErrorHandler";
 import cloudinary from 'cloudinary'
 import { createCourse } from "../services/course.service";
+import CourseModel from "../Models/course.Model";
 
 
 
-
-export const uploadCourse = CatchAsyncErrors(async(req:Request, res:Response, next:NextFunction)=>{
+// create course
+export const uploadCourse = CatchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
     try {
         const data = req.body;
         const thumbnail = data.thumbnail;
-        if(thumbnail){
+        if (thumbnail) {
             const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
                 folder: "courses"
             });;
             data.thumbnail = {
-                public_id:myCloud.public_id,
-                url:myCloud.secure_url,
+                public_id: myCloud.public_id,
+                url: myCloud.secure_url,
             }
         }
-        createCourse(data,res,next);
-    } catch (error:any) {
+        createCourse(data, res, next);
+    } catch (error: any) {
         return next(new ErrorHandler(error.message, 400));
     }
-})
+});
+
+// edit course
+export const editCourse = CatchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+        const data = req.body;
+        const thumbnail = data.thumbnail;
+
+        if(thumbnail){
+            await cloudinary.v2.uploader.destroy(thumbnail.public_id);
+
+            const myCloud = await cloudinary.v2.uploader.upload(thumbnail,{
+                folder: 'courses',
+            });
+
+            data.thumbnail = {
+                public_id: myCloud.public_id,
+                url: myCloud.secure_url,
+            };
+        }
+
+        const courseId = req.params.id;
+
+        const course = await CourseModel.findByIdAndUpdate(
+            courseId,
+            {
+                $set: data,
+            },
+            { new: true}
+        );
+        res.status(201).json({
+            success: true,
+            course,
+        });
+  } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400));
+    }
+});
--- server/Routes/course.route.ts
+++ server/Routes/course.route.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { authorizeRole, isAuthenticated } from "../Middleware/auth";
-import { uploadCourse } from "../Controllers/course.Controller";
+import { editCourse, uploadCourse } from "../Controllers/course.Controller";
 
 const courseRouter = express.Router();
 
 courseRouter.post('/create-course', isAuthenticated, authorizeRole("admin"), uploadCourse);
+
+courseRouter.put('/edit-course/:id', isAuthenticated, authorizeRole("admin"), editCourse);
 
 
 
