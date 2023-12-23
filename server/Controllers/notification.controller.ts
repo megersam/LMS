0313afd644e9e063f2af4652607d6f016c5664a3
megersam <megersabmuleta01@gmail.com>
2023-12-23 13:20:28 +0300

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   server/Controllers/notification.controller.ts #	modified:   server/package-lock.json #	modified:   server/package.json #

--- server/Controllers/notification.controller.ts
+++ server/Controllers/notification.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { CatchAsyncErrors } from "../Middleware/CatchAsyncErrors";
 import ErrorHandler from "../utils/ErrorHandler";
 import NotificationModel from "../Models/notificationModel";
+import cron from 'node-cron';
 
 
 // get all notifications  only for admins
@@ -41,4 +42,12 @@ export const updateNotification = CatchAsyncErrors(async(req:Request, res:Respon
     } catch (error:any) {
         return next(new ErrorHandler(error.message, 500));
     }
+});
+
+
+// delete notification with over 30 days and read status
+cron.schedule("0 0 0 * * * ", async()=>{
+    const thirtyDaysAgo = new Date(Date.now() -30 * 24 * 60 * 60 * 1000);
+    await NotificationModel.deleteMany({status:'read', createdAt: {$lt: thirtyDaysAgo}});
+    console.log('Deleted read notifications')
 })
